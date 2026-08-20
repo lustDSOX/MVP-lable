8c113b4e4a8c0a34cd9f2877f25fcb9351d36feb
lustDSOX <96828722+lustDSOX@users.noreply.github.com>
2026-08-21 00:00:34 +0300

    fix: restore ArtistReleaseView + TrackForm GenrePicker DnD stats lyrics

--- frontend/mvp_lable/src/components/dashboard/ArtistReleaseView.vue
+++ frontend/mvp_lable/src/components/dashboard/ArtistReleaseView.vue
@@ -1,5 +1,1 @@
-<template>
-  <section v-if="track" class="space-y-4 border-2 border-[#39FF14] bg-[#050505] p-4 sm:p-6">
-    <p>RESTORING - see next commit</p>
-  </section>
-</template>
+SEE_ARTIFACTS
